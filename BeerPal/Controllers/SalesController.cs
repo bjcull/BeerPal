9ab0faec1ffbf3ee06551a32e1666e79e22761fd
Ben Cull <bjcull@gmail.com>
2016-08-28 16:30:41 +0200

    Added sales and subcriptions lists and refunds/cancellations

---
+++ BeerPal/Controllers/SalesController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PayPal.Api;
+
+namespace BeerPal.Controllers
+{
+    public class SalesController : Controller
+    {
+        public ActionResult Index()
+        {
+            var apiContext = GetApiContext();
+
+            var sales = Payment.List(apiContext);
+
+            return View(sales);
+        }
+
+        public ActionResult Refund(string saleId)
+        {
+            var apiContext = GetApiContext();
+
+            var sale = new Sale()
+            {
+                id = saleId
+            };
+
+            // A refund with no details refunds the entire amount.
+            var refund = sale.Refund(apiContext, new Refund());
+
+            return RedirectToAction("Index");
+        }
+
+        private APIContext GetApiContext()
+        {
+            // Authenticate with PayPal
+            var config = ConfigManager.Instance.GetProperties();
+            var accessToken = new OAuthTokenCredential(config).GetAccessToken();
+            var apiContext = new APIContext(accessToken);
+            return apiContext;
+        }
+    }
+}
---
+++ BeerPal/Controllers/SubscribersController.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BeerPal.Models;
+using Microsoft.AspNet.Identity.Owin;
+using PayPal.Api;
+
+namespace BeerPal.Controllers
+{
+    public class SubscribersController : Controller
+    {
+        private ApplicationDbContext _dbContext => HttpContext.GetOwinContext().Get<ApplicationDbContext>();
+
+        public ActionResult Index()
+        {
+            var subscriptions = _dbContext.Subscriptions.OrderByDescending(x => x.StartDate).Take(50).ToList();
+
+            return View(subscriptions);
+        }
+
+        public ActionResult Details(string id)
+        {
+            var apiContext = GetApiContext();
+
+            var agreement = Agreement.Get(apiContext, id);
+
+            return View(agreement);
+        }
+
+        public ActionResult Suspend(string id)
+        {
+            var apiContext = GetApiContext();
+
+            Agreement.Suspend(apiContext, id, new AgreementStateDescriptor()
+            {
+                note = "Suspended"
+            });
+
+            return RedirectToAction("Details", new {id = id});
+        }
+
+        public ActionResult Reactivate(string id)
+        {
+            var apiContext = GetApiContext();
+
+            Agreement.ReActivate(apiContext, id, new AgreementStateDescriptor()
+            {
+                note = "Reactivated"
+            });
+
+            return RedirectToAction("Details", new {id = id});
+        }
+
+        public ActionResult Cancel(string id)
+        {
+            var apiContext = GetApiContext();
+
+            Agreement.Cancel(apiContext, id, new AgreementStateDescriptor()
+            {
+                note = "Cancelled"
+            });            
+
+            return RedirectToAction("Details", new {id = id});
+        }
+
+
+
+        private APIContext GetApiContext()
+        {
+            // Authenticate with PayPal
+            var config = ConfigManager.Instance.GetProperties();
+            var accessToken = new OAuthTokenCredential(config).GetAccessToken();
+            var apiContext = new APIContext(accessToken);
+            return apiContext;
+        }
+    }
+}
